37990cd0576aeb4175150aea08ac1954e9463e82
pandanax <ashishkin@advzoom.com>
2025-05-04 12:27:17 +0300

    test

--- miniapp/web/src/App.tsx
+++ miniapp/web/src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Мандала</h1>
+      <h1>Моя Мандала</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
            число нажатий = {count}
